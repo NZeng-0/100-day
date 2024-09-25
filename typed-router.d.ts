9cc10dd86dfccdc77cf9448d853296472224508f
xiaofan <ningfengzeng@gmail.com>
2024-09-25 23:18:20 +0800

    day 2

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -20,5 +20,6 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/001': RouteRecordInfo<'/001', '/001', Record<never, never>, Record<never, never>>,
+    '/002': RouteRecordInfo<'/002', '/002', Record<never, never>, Record<never, never>>,
   }
 }
